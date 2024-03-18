555afda375f777e0495eee164e8e3fd2a0d36b89
shingokataoka <shingo.kataoka777@gmail.com>
2024-03-18 23:30:02 +0900

    経歴を追加

--- app/resources/js/Pages/Other/Index.jsx
+++ app/resources/js/Pages/Other/Index.jsx
@@ -1,7 +1,10 @@
+import React from "react"
+
 import { usePage } from "@inertiajs/react"
 import { css } from "@emotion/react"
 import { useMediaQuery } from "@mui/material"
 import { defaultTheme, DefaultThemeProvider } from "@/Components/defaultThemeProvider"
+import nl2br from "@/Functions/nl2br"
 
 import VideoImage from "@/Components/Other/Indexs/VideoImage"
 import Video from "@/Components/Other/Indexs/Video"
@@ -18,7 +21,45 @@ export default function Index () {
 
     const domain = usePage().props.domain
 
-    // スマホ幅の場合の
+
+    const aborts = [
+        [`2008年 3月`, `近畿大学理工学部機械工学科を卒業。`],
+
+        [`2008年 4月`, `自動二輪車パーツの製造会社に就職する。`],
+
+        [`2008年 8月`, `音楽家を目指すために退社し、音楽活動を始める。`],
+
+        [`2010年 3月`, `貯金が尽きたためスポーツ量販店に臨時社員として就職し、並行して音楽活動を続ける。
+        主に自転車の整備や修理と販売を担当し、後に自転車整備士・自転車技士の資格を取る。
+
+        この時期、インターネット上に音楽や演奏動画などを公開したり交流を持ったことで、webの凄さと可能性を感じ、興味を持ち始めるきっかけとなる。`],
+
+        [`2015年10月`, `音楽を諦め、簡単なwebサイト制作の練習を始める。`],
+
+        [`2016年 3月`, `スポーツ量販店を退社。自分の力で収益を上げることを目標に、webサービス制作を個人で始める。`],
+
+        [`2018年 6月`, `貯金が尽きたため切削工具の製造会社に就職。並行してwebサービス制作を続ける。`],
+
+        [`2019年 2月`, `業務上 避けられない危険性へのリスクを感じたため転職、前回勤めていたスポーツ量販店に再び臨時社員として務める（現在の職場）。
+        webサービス制作も継続していく。`],
+
+        [`2019年12月`, `複数作ったwebサイトで十分な収益を上げられず挫折、自分には特別な才能は一つもないことをようやく確信する。`],
+
+        [`2020年 1月`, `スキル学習・実践を堅実に積み重ねるほど結果がでるプログラミングの性質が、自分の性に合っている事に気づく。
+
+        プログラマとして就職する事を目標に、基礎から学習をやり直す。`],
+
+        [`2022年 1月`, `Dockerやgithub、Laravel+Inertia+vue3までの基礎学習を終え、腕試しに職場の業務を効率化するwebアプリの制作を始める。`],
+
+        [`2022年12月`, `業務アプリが完成。自分や同僚の業務の効率が1/3になり、意味のあるwebアプリを制作できたと実感する。`],
+
+        [`2023年 1月`, `未学習だったReactを中心に、学習を続ける。`],
+
+        [`2023年 6月`, `学習を続けつつ、徐々にポートフォリオの作成も始める。`],
+
+        [`2024年 3月`, `ポートフォリオが完成。プログラマになるため就活の準備を始める。`],
+    ]
+
 
     return (<DefaultThemeProvider>
         <style>{`
@@ -38,9 +79,10 @@ export default function Index () {
         <div css={css`
             margin:0 auto;
             background: #333;
-            padding:64px 16px 64px 16px;
             max-width: 800px;
         `}>
+
+
             {/* 動画と光る文字とパーティクル */}
             <div css={css`
                 position:relative;
@@ -55,6 +97,8 @@ export default function Index () {
 
             </div>
 
+
+
             {/* サイト説明 */}
             <Stack
                 direction="column"
@@ -62,7 +106,8 @@ export default function Index () {
                 alignItems="center"
                 spacing={4}
                 css={css`
-                    margin-top:32px;
+                    padding:32px 16px;
+                    background: linear-gradient(90deg, #333, #555);
                 `}
             >
                 <h2>作成したサイト 一覧</h2>
@@ -83,61 +128,127 @@ export default function Index () {
 
 
 
+            {/* 各サイトのgithubへのリンク */}
+            <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={2}
+                css={css`
+                    padding:32px 16px;
+                    background: linear-gradient(90deg, #555, #333);
+                    * { text-align:center; }
+                    a {
+                        max-width:630px;
+                        width:100%;
+                        background: #333;
+                        border: 1px #888 solid;
+                        border-radius: 8px;
+                        padding: 8px;
+                        font-size:0.9rem;
+                        :hover {
+                            background: #505050;
+                        }
+                    }
+                `}
+            >
+
+                <h2>ポートフォリオのgithub</h2>
+
+                <p>当サイトは全てdockerを用いて作成しています。</p>
+
+                <a href="https://github.com/shingokataoka/laravels__index">
+                    laravels__index<br />
+                    現在ページのgithubです。
+                </a>
+
+                <a href="https://github.com/shingokataoka/laravels__sk_marche">
+                    laravels__sk_marche<br />
+                    上記「Stripe（テスト環境）を 決済導入したECサイト」の<br />
+                    guthubです。
+                </a>
+                <a href="https://github.com/shingokataoka/laravels__ucrm">
+                    laravels__ucrm<br />
+                    上記「uCRM（顧客管理）サイト」の<br />
+                    guthubです。
+                </a>
+                <a href="https://github.com/shingokataoka/laravels__db_web_ssl">
+                    laravels__db_web_ssl<br />
+                    上記の複数サイトは、一つのDBコンテナとWEBコンテナを共有で使っています。<br/>
+                    （vpsのメモリ制約対策です。）<br />
+                    <br />
+                    この「DBコンテナとWEBコンテナとSSL化」のgithubです。
+                </a>
+            </Stack>
+
+
+
+            {/* 経歴 */}
+            <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={2}
+                css={css`
+                    padding:32px 16px;
+                    background: linear-gradient(90deg, #333, #346);
+                `}
+            >
+
+                <h2>経歴</h2>
+
+                {/* スマホでの経歴表示 */}
+                { !isMobile? '' :
+                    <Stack
+                        direction="column"
+                        justifyContent="flex-start"
+                        alignItems="center"
+                        spacing={2}
+                        css={css`
+                            text-align: center;
+                            .date {
+                                width:calc(100% - 32px);
+                                border-top: 1px #fff3 solid;
+                            }
+                        `}
+                    >
+                        { aborts.map( (row, index) => (<React.Fragment key={index}>
+                            <div className="date">{ row[0] }</div>
+                            <div>{ nl2br(row[1]) }</div>
+                        </React.Fragment>) ) }
+                    </Stack>
+                }
+
+
+                {/* パソコンでの経歴表示 */}
+                { isMobile? '' :
+                    <table>
+                        <tbody css={css`
+                            td {
+                                // border: 1px #555 solid;
+                                padding:16px 0px;
+                                vertical-align:top;
+                            }
+                            td.date{
+                                width: 130px;
+                            }
+                        `}>
+                        { aborts.map( (row, index) => (<tr key={index}>
+                                <td className="date">{row[0]}</td>
+                                <td>{ nl2br(row[1]) }</td>
+                            </tr>) ) }
+                        </tbody>
+                    </table>
+                }
+
+            </Stack>
+
         </div>
 
 
 
-        <Stack
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={2}
-            css={css`
-                margin-top:32px;
-               padding:16px 16px 64px 16px;
-                * { text-align:center; }
-                a {
-                    max-width:630px;
-                    width:100%;
-                    background: #333;
-                    border: 1px #888 solid;
-                    border-radius: 8px;
-                    padding: 8px;
-                    font-size:0.9rem;
-                    :hover {
-                        background: #505050;
-                    }
-                }
-            `}
-        >
 
-            <h2>ポートフォリオのgithub</h2>
 
-            <p>当サイトは全てdockerを用いて作成しています。</p>
-
-            <a href="https://github.com/shingokataoka/laravels__index">
-                laravels__index<br />
-                現在ページのgithubです。
-            </a>
-
-            <a href="https://github.com/shingokataoka/laravels__sk_marche">
-                laravels__sk_marche<br />
-                上記「Stripe（テスト環境）を 決済導入したECサイト」の<br />
-                guthubです。
-            </a>
-            <a href="https://github.com/shingokataoka/laravels__ucrm">
-                laravels__ucrm<br />
-                上記「uCRM（顧客管理）サイト」の<br />
-                guthubです。
-            </a>
-            <a href="https://github.com/shingokataoka/laravels__db_web_ssl">
-                laravels__db_web_ssl<br />
-                上記の複数サイトは、一つのDBコンテナとWEBコンテナを共有で使っています。<br/>
-                （vpsのメモリ制約対策です。）<br />
-                <br />
-                この「DBコンテナとWEBコンテナとSSL化」のgithubです。
-            </a>
-        </Stack>
 
     </DefaultThemeProvider>)
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -26,19 +26,22 @@ use Inertia\Inertia;
 // });
 
 
+// トップページ
 Route::get('/', function () {
     return Inertia::render('Other/Index');
 })->name('index');
 
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
